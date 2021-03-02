fd1ef741107f7602764e3931bda7ee335273634b
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-03-02 12:50:41 -0300

    Adds validation to Question and Tag - Question: title and description needs to be present - Question: adds mininum and maximum length - Tag: name needs to be present - Tag: adds maximum length

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :tags
+
+  validates :title, length: {minimum:10, maximum:100}, presence: :true
+  validates :description, length: {minimum:10, maximum:1000}, presence: :true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
   belongs_to :question
+
+  validates :name, length: {maximum:100}, presence: :true
 end
